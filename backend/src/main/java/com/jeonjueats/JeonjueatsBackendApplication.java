4814f1171ca90bdad0d32536ab18b94ab2f12c0e
Jihyeok <qowlgur121@gmail.com>
2025-06-06 12:56:05 +0900

    feat: 프로젝트 초기 설정 및 저장소 구성 완료

---
+++ backend/src/main/java/com/jeonjueats/JeonjueatsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jeonjueats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeonjueatsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeonjueatsBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jeonjueats/JeonjueatsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeonjueats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeonjueatsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
